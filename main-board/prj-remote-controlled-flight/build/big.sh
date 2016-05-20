1c97fac19cc8df8e100c619b2fa838bf36a3d348
fernandosgoncalves <fernandosilgon@gmail.com>
2016-05-20 12:45:56 -0300

    Remove build files

--- main-board/prj-remote-controlled-flight/build/big.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-make -j4
-
-scp provant_main_v1 root@192.168.0.110:/home/VANT
--- main-board/prj-remote-controlled-flight/build/subsystems/subsys_DataProcessing/CMakeFiles/subsys_DataProcessing_bundle.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../../../subsystems/subsys_DataProcessing/build/libsubsys_DataProcessing_bundle.a"
-)
